a5d9612cbb42d8f5adfdb1618085604424327244
sveta987 <svetaantonyan00@gmail.com>
2023-07-27 00:21:27 +0400

    bug fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="bg-[#FFFEF4] vrdznagir">
-    <div class="relative" >
-      <img src="../src/assets/img/bg-image.png" alt="bg-image">
+    <div class="relative max-md:h-[100vh]">
+      <img src="../src/assets/img/bg-image.png" alt="bg-image" class="max-md:h-[100vh] max-md:object-cover lg:h-fit lg:w-full">
       <div class="absolute top-0 left-0 right-0 z-10 h-full backgroundOverlay">
         <div class="mt-[15%] text-center text-[#98904A] z-30 text-[85px] max-sm:text-[30px] max-md:text-[40px]">
-          <p class="whitespace-nowrap md:whitespace-normal">
+          <p class="whitespace-nowrap md:whitespace-normal max-md:!mt-[80%]">
             Հարությունյան եղբայրների <br>
             Սուրբ մկրտություն
           </p>
           <p>
             05.08.2023
           </p>
-          <img src="../src/assets/img/arrow.svg" alt="arrow" class="mx-auto mt-[8%] bounce w-[4%]">
+          <img src="../src/assets/img/arrow.svg" alt="arrow" class="mx-auto mt-[8%] bounce w-[4%] max-md:w-[10%] max-md:mt-[80%] max-sm:mt-[50%]" @click="scrollToSeeDetails">
         </div>
       </div>
     </div>
-    <div class="py-[110px] max-md:[150px]">
+    <div id="invitation" class="py-[110px] max-md:py-[30px]">
       <p class="text-[#98904A] text-center text-[58px] max-sm:text-[18px] max-md:text-[25px]">
         Սիրելի բարեկամներ հրավիրում ենք Ձեզ
         մասնակցելու Հարությունյան եղբայրների սուրբ մկրտության
@@ -34,7 +34,7 @@
             Սրբոց նահատակաց եկեղեցի
           </p>
           <p class="text-[#98904A] text-center text-[58px] max-sm:text-[18px] max-md:text-[25px] leading-[150%]">16։30</p>
-          <a href="https://www.google.com/maps/place/%D5%8D%D6%80%D5%A2%D5%B8%D6%81+%D5%86%D5%A1%D5%B0%D5%A1%D5%BF%D5%A1%D5%AF%D5%A1%D6%81+%D5%A5%D5%AF%D5%A5%D5%B2%D5%A5%D6%81%D5%AB/@40.0885096,44.5388172,16z/data=!4m10!1m2!2m1!1z1LTVodW-1avVqdWh1bfVpdW21asg1b3WgNWi1bjWgSDVttWh1bDVodW_1aHVr9Wh1oEg1aXVr9Wl1bLVpdaB1as!3m6!1s0x406ab97a4e9ba4c5:0x6dfbb42b709c7dbe!8m2!3d40.0885096!4d44.5483444!15sCkHUtNWh1b7Vq9Wp1aHVt9Wl1bbVqyDVvdaA1aLVuNaBINW21aHVsNWh1b_VodWv1aHWgSDVpdWv1aXVstWl1oHVq5IBD2FybWVuaWFuX2NodXJjaOABAA!16s%2Fg%2F11srysmmn3?entry=ttu"
+          <a href="https://www.google.com/maps/search/%D4%B4%D5%A1%D5%BE%D5%AB%D5%A9%D5%A1%D5%B7%D5%A5%D5%B6%D5%AB+%D5%BD%D6%80%D5%A2%D5%B8%D6%81+%D5%B6%D5%A1%D5%B0%D5%A1%D5%BF%D5%A1%D5%AF%D5%A1%D6%81+%D5%A5%D5%AF%D5%A5%D5%B2%D5%A5%D6%81%D5%AB/@40.2145744,44.4895791,18z?entry=ttu"
              class="text-[40px] max-md:text-[20px] max-sm:text-[15px] underline" target="_blank">
             Տեսնել հասցեն
           </a>
@@ -44,7 +44,7 @@
         <p class="whitespace-nowrap md:whitespace-normal">
           Հանդիսությունների սրահ
         </p>
-        <img src="./assets/img/wine-glasses.png" alt="wine_glases" class="mx-auto max-sm:w-[25%] max-md:w-[30%]">
+        <img src="./assets/img/wine-glass.png" alt="wine_glases" class="mx-auto max-sm:w-[25%] max-md:w-[30%]">
 
         <div class="flex flex-col gap-[5%] mt-[10%]">
           <p class="text-[#98904A] text-center text-[58px] max-sm:text-[18px] max-md:text-[25px] leading-[150%]">
@@ -63,21 +63,25 @@
       Սիրով սպասում ենք ձեզ
     </p>
     
-    <p class="text-[25px] max-md:text-[15px] max-sm:text-[10px] mt-[5%] text-[#98904A] text-center flex items-center gap-5 justify-center flex-wrap">
+    <p class="text-[25px] max-md:text-[15px] max-sm:text-[10px] mt-[5%] text-[#98904A] text-center ">
       Եթե ցանկանում ես ունենալ նման կայք անցիր հղումով
-      <a href="https://www.instagram.com/onlineinvitationcards_/" target="_blank" class="flex items-center gap-2 text-sm">
-        <img src="./assets/img/instagram-logo.png" alt="" class="w-[20px]">
+      <a href="https://www.instagram.com/onlineinvitationcards_/" target="_blank" class="flex items-center gap-2 text-sm justify-center">
+        <img src="./assets/img/instagram.png" alt="" class="w-[15px] text-[#98904A]">
         onlineinvitationcards
       </a>
     </p>
     <div class="absolute top-[350%] left-0 max-md:top-[200%] max-sm:top-[150%]">
       <img src="./assets/img/angel-l.png" alt="angel" class="max-md:w-[40%] max-sm:w-[30%] md:w-[45%] lg:w-[100%]">
     </div>
-      <img src="./assets/img/angel-r.png" alt="angel" class="max-md:h-[40%] max-sm:h-[30%] md:h-[45%] lg:h-[80%] absolute top-[270%] !right-0 max-md:top-[150%] max-sm:top-[120%] w-fit">
+      <img src="./assets/img/angel-r.png" alt="angel" class="max-md:h-[40%] max-sm:h-[30%] md:h-[45%] lg:h-[80%] absolute top-[270%] !right-0 max-md:top-[200%] max-sm:top-[170%] w-fit">
   </div>
 </template>
 
 <script setup>
+const scrollToSeeDetails = () => {
+  let access = document.getElementById("invitation");
+  access.scrollIntoView({behavior: 'smooth'}, true);
+}
 </script>
 
 <style scoped>
